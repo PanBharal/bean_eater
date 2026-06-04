61a395e44c28beae44940aec7ebe202eb04a4dba
PanBharal <panyanyang13@outlook.com>
2026-06-04 23:56:09 +0800

    feat: bean设置存在时间 超时扣分/扣血

--- src/game.cpp
+++ src/game.cpp
@@ -27,58 +27,48 @@ static type openAsset(std::filesystem::path path)
 
 Game::Game()
     : window(sf::VideoMode({ 1080, 770 }), L"吃豆人 - 我吃故我在！")
-    , font(open_asset<sf::Font>("assets/song.ttf"))
-    , bgm(open_asset<sf::Music>("assets/rifle.mp3"))
-    , eat_buffer(load_asset<sf::SoundBuffer>("assets/eat.ogg"))
-    , hurt_buffer(load_asset<sf::SoundBuffer>("assets/hurt.ogg"))
-    , death_buffer(load_asset<sf::SoundBuffer>("assets/death.wav"))
-    , menu_back(load_asset<sf::Texture>("assets/menu_back.png"))
-    , eater(load_asset<sf::Texture>("assets/eater.png"))
-    , sprite(eater)
-    , text(font)
+    , font(openAsset<sf::Font>("assets/song.ttf"))
+    , menu_bgm(openAsset<sf::Music>("assets/I eat so I'm here.mp3"))
+    , bgm(openAsset<sf::Music>("assets/rifle.mp3"))
+    , eat_buffer(loadAsset<sf::SoundBuffer>("assets/eat.ogg"))
+    , hurt_buffer(loadAsset<sf::SoundBuffer>("assets/hurt.ogg"))
+    , death_buffer(loadAsset<sf::SoundBuffer>("assets/death.wav"))
+    , bean_buffer(loadAsset<sf::SoundBuffer>("assets/pong.wav"))
+    , init_menu_back(loadAsset<sf::Texture>("assets/menu_back.png"))
+    , eater(loadAsset<sf::Texture>("assets/eater.png"))
+    , pacman(eater)
+    , menu_text(font)
+    , over_title(font)
+    , over_title_2(font)
+    , over_text(font)
     , print_score(font)
     , print_hp(font)
+    , over_score(font)
     , eat(eat_buffer)
     , hurt(hurt_buffer)
     , death(death_buffer)
+    , bean_move(bean_buffer)
     , rand_gen(seed())
 {
     window.setFramerateLimit(60);
     window.setKeyRepeatEnabled(false);
 
-    back.setSize({ 1080, 770 });
-    back.setTexture(&menu_back);
-    back.setPosition({ 0, 0 });
+    initText();
 
-    text.setString(L"吃个球！");
-    text.setCharacterSize(200);
-    text.setOrigin(text.getGlobalBounds().getCenter());
-    text.setPosition({ 540, 200 });
-    text.setFillColor(sf::Color::Yellow);
-    text.setOutlineColor({ 170, 136, 0 });
-    text.setOutlineThickness(3.f);
+    menu_bgm.setVolume(10);
+    menu_bgm.setLooping(true);
 
-    print_score.setCharacterSize(50);
-    print_score.setFillColor(sf::Color::White);
-    print_score.setOutlineColor({ 67, 67, 67 });
-    print_score.setOutlineThickness(3.f);
-
-    print_hp.setCharacterSize(50);
-    print_hp.setPosition({ 540, 0 });
-    print_hp.setFillColor(sf::Color::Red);
-    print_hp.setOutlineColor(sf::Color::White);
-    print_hp.setOutlineThickness(3.f);
-
-    bgm.setVolume(60);
+    bgm.setVolume(40);
     bgm.setLooping(true);
 
     eat.setVolume(800);
     hurt.setVolume(800);
     death.setVolume(1000);
+    bean_move.setVolume(800);
 
-    sprite.setOrigin(sprite.getGlobalBounds().getCenter());
-    sprite.setPosition({ 540, 360 });
-    sprite.setRotation(sf::degrees(0.f));
+    pacman.setOrigin(pacman.getGlobalBounds().getCenter());
+    pacman.setPosition({ 540, 360 });
+    pacman.setRotation(sf::degrees(0.f));
 
     bean.setRadius(15.f);
     std::uniform_real_distribution<float> distX(0.f, 1080.f - 2 * bean.getRadius());
@@ -87,6 +77,59 @@ Game::Game()
     bean.setFillColor({ 108, 209, 46 });
     bean.setOutlineThickness(-5.f);
     bean.setOutlineColor({ 181, 230, 29 });
+}
+
+void Game::initText()
+{
+    menu_text.setString(L"吃个球！");
+    menu_text.setCharacterSize(200);
+    menu_text.setOrigin(menu_text.getGlobalBounds().getCenter());
+    menu_text.setPosition({ 540, 200 });
+    menu_text.setFillColor({ 67, 67, 67 });
+    menu_text.setOutlineColor(sf::Color::White);
+    menu_text.setOutlineThickness(2.f);
+
+    print_hp.setString("HP: " + std::to_string(hp));
+    print_hp.setCharacterSize(50);
+    print_hp.setOrigin(print_hp.getGlobalBounds().getCenter());
+    print_hp.setPosition({ 1080 - print_hp.getGlobalBounds().size.x - 60, print_hp.getGlobalBounds().size.y / 2 + 10 });
+    print_hp.setFillColor(sf::Color::Red);
+    print_hp.setOutlineColor(sf::Color::White);
+    print_hp.setOutlineThickness(2.f);
+
+    print_score.setString(L"得分：" + std::to_wstring(score));
+    print_score.setCharacterSize(50);
+    print_score.setOrigin(print_score.getGlobalBounds().getCenter());
+    print_score.setPosition({ print_score.getGlobalBounds().size.x + 10, print_score.getGlobalBounds().size.y / 2 + 10 });
+    print_score.setFillColor(sf::Color::White);
+    print_score.setOutlineColor({ 67, 67, 67 });
+    print_score.setOutlineThickness(2.f);
+
+    over_title.setString(L"由于你太爱吃墙\n被娇羞的熟石灰\n腐蚀了脑子......");
+    over_title.setCharacterSize(100);
+    over_title.setOrigin(over_title.getGlobalBounds().getCenter());
+    over_title.setPosition({ 540, 230 });
+    over_title.setFillColor(sf::Color::Red);
+
+    over_title_2.setString(L"民以食为天！！\nbro怎么连食物都追不上...");
+    over_title_2.setCharacterSize(80);
+    over_title_2.setOrigin(over_title.getGlobalBounds().getCenter());
+    over_title_2.setPosition({ 600, 330 });
+    over_title_2.setFillColor(sf::Color::Red);
+
+    over_score.setString(L"最终得分：" + std::to_wstring(score));
+    over_score.setCharacterSize(50);
+    over_score.setOrigin(over_score.getGlobalBounds().getCenter());
+    over_score.setPosition({ 540, 560 });
+    over_score.setFillColor(sf::Color::White);
+    over_score.setOutlineColor({ 67, 67, 67 });
+    over_score.setOutlineThickness(2.f);
+
+    over_text.setString(L"<R> 继续  <ESC> 退出");
+    over_text.setCharacterSize(30);
+    over_text.setOrigin(over_text.getGlobalBounds().getCenter());
+    over_text.setPosition({ 540, 690 });
+    over_text.setFillColor(sf::Color::White);
 }
 
 void Game::run()
@@ -116,6 +159,12 @@ void Game::handle_event(const sf::Event::KeyPressed& key)
         window.close();
     } else if (key.code == sf::Keyboard::Key::Space) {
         is_menu = false;
+    } else if (key.code == sf::Keyboard::Key::R) {
+        score = 0;
+        hp = 3;
+        init_game = false;
+        is_over = false;
+        init_over = false;
     }
 }
 
@@ -123,98 +172,22 @@ void Game::render()
 {
     if (is_menu) {
         window.clear(sf::Color::White);
-        window.draw(back);
-        window.draw(text);
-    } else if (!is_menu && !game_over) {
+        window.draw(menu_back);
+        window.draw(menu_text);
+    } else if (!is_menu && !is_over) {
         window.clear({ 136, 171, 218 });
         window.draw(bean);
-        window.draw(sprite);
+        window.draw(pacman);
         window.draw(print_score);
         window.draw(print_hp);
-    } else if (game_over) {
+    } else if (is_over) {
         window.clear(sf::Color::Black);
-        window.draw(print_hp);
-        window.draw(print_score);
+        if (score <= 0)
+            window.draw(over_title_2);
+        else
+            window.draw(over_title);
+        window.draw(over_score);
+        window.draw(over_text);
     }
     window.display();
-}
-
-void Game::update()
-{
-
-    if (!is_menu && !game_over) {
-        if (!init_game) {
-            bgm.play();
-            init_game = true;
-        }
-
-        float delta_time = clock.restart().asSeconds();
-        control(delta_time, 1000.f);
-
-        print_score.setString(L"得分：" + std::to_wstring(score));
-        print_hp.setString("HP: " + std::to_string(hp));
-
-        if (sprite.getGlobalBounds().findIntersection(bean.getGlobalBounds())) {
-            eat.play();
-            std::uniform_real_distribution<float> newX(0.f, 1080.f - 2 * bean.getRadius());
-            std::uniform_real_distribution<float> newY(0.f, 720.f - 2 * bean.getRadius());
-            bean.setPosition({ newX(rand_gen), newY(rand_gen) });
-            score++;
-        }
-
-        if (out_boundary()) {
-            sprite.setPosition({ 540, 360 });
-            hurt.play();
-            hp--;
-        }
-    }
-    if (hp <= 0 && !game_over) {
-        game_over = true;
-        bgm.stop();
-        death.play();
-        print_hp.setString(L"民以食为天！！\n由于你太爱吃墙\n被娇羞的熟石灰\n腐蚀了脑子......");
-        print_hp.setCharacterSize(100);
-        print_hp.setOrigin(print_hp.getGlobalBounds().getCenter());
-        print_hp.setPosition({ 1080, 240 });
-        print_score.setString(L"最终得分：" + std::to_wstring(score) + L"\n\n按ESC以退出");
-        print_score.setOrigin(print_score.getGlobalBounds().getCenter());
-        print_score.setPosition({ 540, 590 });
-    }
-}
-
-void Game::control(float delta_time, float speed)
-{
-    float move_speed = speed * delta_time;
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left) || sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) {
-        sprite.setRotation(sf::degrees(180.f));
-        sprite.move({ -move_speed, 0.f });
-    }
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right) || sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) {
-        sprite.setRotation(sf::degrees(360.f));
-        sprite.move({ move_speed, 0.f });
-    }
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up) || sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) {
-        sprite.setRotation(sf::degrees(-90.f));
-        sprite.move({ 0.f, -move_speed });
-    }
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down) || sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) {
-        sprite.setRotation(sf::degrees(90.f));
-        sprite.move({ 0.f, move_speed });
-    }
-}
-
-bool Game::out_boundary()
-{
-    sf::Vector2f pos = sprite.getPosition();
-    float eater_origin = sprite.getGlobalBounds().size.x / 2.f;
-    if (pos.x + eater_origin >= 1080) {
-        return true;
-    } else if (pos.x - eater_origin <= 0) {
-        return true;
-    } else if (pos.y + eater_origin >= 770) {
-        return true;
-    } else if (pos.y - eater_origin <= 0) {
-        return true;
-    } else
-        return false;
 }
